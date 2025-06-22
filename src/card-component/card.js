1c56bf7e2b262a18a45d194f4203bf47835e7a12
leslie628 <lf24aap@herts.ac.uk>
2025-06-22 21:52:17 +0100

    icon changes git docs

--- src/card-component/card.js
+++ src/card-component/card.js
@@ -1,6 +1,6 @@
 import "./card.css";
 import { Button } from "../button";
-import { Code, ExternalLink, VideoOff } from "lucide-react";
+import { Code, ExternalLink, VideoOff, FileText } from "lucide-react";
 const Card = ({ project }) => {
     return <div className="card">
         <h2 className="text-2xl font-semibold mb-2">{project.title}</h2>
@@ -10,9 +10,10 @@ const Card = ({ project }) => {
                 <a href={project.github} target="_blank" rel="noopener noreferrer">
                     <Code className="mr-2 h-4 w-4" /><span className="link-text">GitHub</span>
                 </a>
-                |
+            </Button>
+            <Button variant="outline" className="button-link" asChild>
                 <a href={project.docs} target="_blank" rel="noopener noreferrer">
-                    <Code className="mr-2 h-4 w-4" /><span className="link-text">Docs</span>
+                    <FileText className="mr-2 h-5 w-5" /><span className="link-text">Github Docs</span>
                 </a>
             </Button>
             <Button variant="outline" className="button-link" asChild>
